4730b6679c7f5c2df2c42fe95fdcd45fe17ba68b
Melvin Sureshbabu <45182146+Melv1nS@users.noreply.github.com>
2021-03-01 15:54:49 -0600

    updated tilemap

---
+++ Assets/Map/Hex Set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Hex Set" tilewidth="32" tileheight="32" tilecount="162" columns="9">
+ <image source="../Tilesets/v01/Hex_v01_grid.png" width="288" height="576"/>
+</tileset>
