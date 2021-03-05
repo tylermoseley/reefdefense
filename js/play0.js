cfbfd65432eac9d086dfb9151d6a29e972ccc2cb
Tyler Moseley <tylermoseley@utexas.edu>
2021-03-05 00:04:49 -0600

    added wasd movement, gameBoard datasheet, coral Class, and coral placements

--- js/play0.js
+++ js/play0.js
@@ -14,7 +14,6 @@ playState0 = {
         map.addTilesetImage('Water');
 
         layer = map.createLayer('Tile Layer 1');
-
         layer.resizeWorld();
 
         //  hover box
@@ -26,10 +25,22 @@ playState0 = {
         game.input.addMoveCallback(updateMarker, this);
 
         // call getTileProperties function when tile is clicked
-        game.input.onDown.add(getTileProperties, this);
+        game.input.onDown.add(clickHandler, this);
 
         // set cursors variable to keyboard cursor input
         cursors = game.input.keyboard.createCursorKeys();
+
+        wasd = {
+            w: game.input.keyboard.addKey(Phaser.Keyboard.W),
+            s: game.input.keyboard.addKey(Phaser.Keyboard.S),
+            a: game.input.keyboard.addKey(Phaser.Keyboard.A),
+            d: game.input.keyboard.addKey(Phaser.Keyboard.D),
+        };
+
+        // mouseWheel to capture scrolling for alternate movement
+        // up/down only in phaser <3.2*
+        // mouseWheel = game.input.mouseWheel;
+
     },
 
     update: function() {
@@ -37,35 +48,79 @@ playState0 = {
         // move camera with cursors with "speed" set
         // (setting to factors of 32 makes it hard to see movement)
         scrollSpd = 8
-        if (cursors.left.isDown) {
+        if (cursors.left.isDown || wasd.a.isDown) {
             game.camera.x -= scrollSpd;
-        } else if (cursors.right.isDown) {
+        } else if (cursors.right.isDown || wasd.d.isDown) {
             game.camera.x += scrollSpd;
         }
-        if (cursors.up.isDown) {
+        if (cursors.up.isDown || wasd.w.isDown) {
             game.camera.y -= scrollSpd;
-        } else if (cursors.down.isDown) {
+        } else if (cursors.down.isDown || wasd.s.isDown) {
             game.camera.y += scrollSpd;
+        }
+
+        // resting animation on for all corals on gameBoard
+        for(i=0; i<=31; i+=1) {
+            for(j=0; j<=31; j+=1) {
+                if (typeof gameBoard[i][j] === 'object') {
+                    if (gameBoard[i][j].id.slice(0, 1) === "c") {
+                        gameBoard[i][j].sprite.animations.play("ripple", 4, true)
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+// create empty 32x32 gameBoard (maybe add dimension variable if board size change)
+gameBoard = [];
+for (i=0; i<=31; i++) {
+    gameBoard.push([])
+    for (j=0; j<=31; j++){
+        gameBoard[i].push("None")
+    }
+}
+// initialize corals list and coralid index sequence
+coralid = "c0"
+
+class Coral {
+    constructor (id) {
+        this.id = id;
+    }
+    locate (tile, gameBoard) {
+        // there is nothing on gameBoard, place coral object
+        // add currency check here later
+        if (gameBoard[tile.x][tile.y] === "None") {
+            this.sprite = game.add.sprite(tile.worldX, tile.worldY, "bubble")
+            this.sprite.animations.add("ripple", [0,3])
+            gameBoard[tile.x][tile.y] = this
+            return 1;
+        // no coral is added for now, change for conflict resolution
+        } else if (gameBoard[tile.x][tile.y] !== "None") {
+            return 0;
         }
     }
 }
 
 // main handler for mouse clicks
-function getTileProperties() {
-    x = layer.getTileX(game.input.activePointer.worldX);
-    y = layer.getTileY(game.input.activePointer.worldY);
+function clickHandler() {
+    pointerX = layer.getTileX(game.input.activePointer.worldX);
+    pointerY = layer.getTileY(game.input.activePointer.worldY);
+    tile = map.getTile(pointerX, pointerY, layer);
 
-    tile = map.getTile(x, y, layer);
+    // create new coral object in corals list under pointer
+    // add type for property for diff corals later
+    tempCoral = new Coral(
+        id = coralid
+    );
 
-    // Note: JSON.stringify will convert the object tile properties to a string
-    currentDataString = JSON.stringify( tile.properties );
-    tile.properties.wibble = true;
+    // locate coral with curent id to game board (increment coralid with prefix if success)
+    if ( tempCoral.locate(tile, gameBoard) ) {coralid = coralid.slice(0,1) + (Number(coralid.slice(1)) + 1)};
 
-    if(currentDataString){
-        game.debug.text('Tile properties: ' + currentDataString + "("+x+","+y+")", 16, 16);
-    } else {
-        game.debug.text("Click on a tile to reveal the properties of the tile", 16, 16);
-    }
+    game.debug.text(coralid, 12, 36)
+    game.debug.text("Tile: world: {"+tile.worldX+","+tile.worldY+"} index: ("+tile.x+","+tile.y+")", 12, 16);
+
 }
 
 // display rectangle on mouse location
