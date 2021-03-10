17fbe92850d3cd5b54262b2bbbb71b2264c81f7f
Melvin Sureshbabu <45182146+Melv1nS@users.noreply.github.com>
2021-03-10 16:07:22 -0600

    added more audio files and sound effects

--- js/menu.js
+++ js/menu.js
@@ -11,7 +11,7 @@ var menuState = {
         // game.add.plugin(PhaserSuperStorage.StoragePlugin);
         // game.add.plugin(PhaserInput.Plugin);
         BG_music = game.add.audio("music");
-        BG_music.play("", 0, .5, true);
+        BG_music.play("", 0, .2, true);
 
         game.add.image(0, 0, 'home-background');
         game.add.button(250, 338, 'PlayButton', startGame, this, 2, 1, 0);
